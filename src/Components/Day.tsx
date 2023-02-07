1bf0a70c99d6492d2e31cfc4864bf454c6873a9a
Aldrin <cleetusaldrin30@gmail.com>
2023-02-07 21:21:05 +0530

    updated the days css

--- src/Components/Day.tsx
+++ src/Components/Day.tsx
@@ -5,14 +5,14 @@ import { DayProps, event } from "../Types/CalendarTypes";
 
 const Day = (props:DayProps) => {
     return ( 
-        <div className="relative rounded-md bg-stone-800  flex flex-col cursor-pointer"  onClick={props.onClick}>
+        <div className="relative rounded-md bg-stone-800  flex flex-col gap-1 cursor-pointer day-highlight"  onClick={props.onClick}>
             <p className=" m-0 absolute bottom-0 right-1">{props.day.format("DD")}</p>
             {
                 props.events?.map(event => {
                     if(event.startDate?.toDateString() === props.day.toDate().toDateString()){
                         console.log(event.startDate, props.day.toDate())
-                        return <div className="relative rounded h-[100%]">
-                            <h3 className="absolute ">Hello</h3>
+                        return <div className="relative rounded  h-[100%] overflow-hidden">
+                            <h3 className="absolute  text-white ">Hello ht hfsjjfslj fsljf sljljl jl</h3>
                             <img className="h-[100%] object-cover" src="temp/test.png" alt="" />
                         </div>
                     }
